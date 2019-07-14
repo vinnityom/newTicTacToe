7b58e0779ed3cd06e573fafc615fab318169cb2e
artem <sukharevartyom@yandex.ru>
2019-07-15 00:26:31 +0300

    add basic functionality

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import update from 'immutability-helper';
+
+import WelcomePage from './components/WelcomePage';
+import GamePage from './components/GamePage';
+
+import {
+  makeEmptyGameField, makeStep, isCellTaken, isGameOver, isTie,
+} from './gameActions';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFormFilled: false,
+      isGameStarted: false,
+      players: {
+        playerX: { name: '', sign: '×', score: 0 },
+        playerO: { name: '', sign: '○', score: 0 },
+      },
+      fieldSize: 3,
+      gameField: [],
+      activePlayer: 'playerX',
+      movesCounter: 0,
+      gameResult: null,
+      winner: null,
+    };
+  }
+
+  checkIfFilled(players) {
+    const { name: playerXName } = players.playerX;
+    const { name: playerOName } = players.playerO;
+    return playerXName !== '' && playerOName !== '';
+  }
+
+  handleNameInput = (player) => ({ target: { value } }) => {
+    const oldPlayers = this.state.players;
+    const newPlayers = update(oldPlayers, { [player]: { name: { $set: value } } });
+    const isFormFilled = this.checkIfFilled(newPlayers);
+    this.setState({ players: newPlayers, isFormFilled });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { fieldSize } = this.state;
+    const gameField = makeEmptyGameField(fieldSize)
+    this.setState({ isGameStarted: true, gameField });
+  }
+
+  handleCellClick = (row, cell) => () => {
+    const { gameField, activePlayer, players, fieldSize, movesCounter } = this.state;
+    const signToPut = players[activePlayer].sign;
+    if (isCellTaken(row, cell, gameField)) {
+      return;
+    }
+    const updatedField = makeStep(row, cell, gameField, signToPut);
+   
+    const getNextActivePlayer = (currentActivePlayer) => {
+      const nextActivePlayer = {
+        playerX: 'playerO',
+        playerO: 'playerX',
+      };
+      return nextActivePlayer[currentActivePlayer];
+    };
+    const nextActivePlayer = getNextActivePlayer(activePlayer);
+    this.setState({ gameField: updatedField, activePlayer: nextActivePlayer, movesCounter: movesCounter + 1 });
+
+    if (isGameOver(gameField, fieldSize, signToPut)) {
+      const winner = activePlayer;
+      const oldScore = players[winner].score;
+
+      const updatedPlayers = update(players, { [winner]: { score: { $set: oldScore + 1 } } });
+      this.setState({ gameResult: 'win', winner, players: updatedPlayers });
+    }
+  }
+
+  componentDidUpdate() {
+    if (isTie(this.state.movesCounter) && !this.state.gameResult) {
+      this.setState({ gameResult: 'tie'});
+    }
+  }
+
+  handleResetButtonClick = () => {
+    const { fieldSize } = this.state;
+    const emptyField = makeEmptyGameField(fieldSize);
+
+    this.setState({
+      gameField: emptyField,
+      winner: null,
+      gameResult: null,
+      movesCounter: 0,
+    });
+  }
+
+  renderWelcomePage() {
+    const { players, isFormFilled } = this.state;
+    const { name: playerXName } = players.playerX;
+    const { name: playerOName } = players.playerO;
+
+    return (
+      <WelcomePage
+        onChange={this.handleNameInput}
+        playerX={playerXName}
+        playerO={playerOName}
+        onSubmit={this.handleSubmit}
+        isFormFilled={isFormFilled}
+      />
+    );
+  }
+
+  renderGamePage() {
+    const { gameField, players, activePlayer, gameResult, winner } = this.state;
+    return (
+      <GamePage
+        gameField={gameField}
+        onCellClick={this.handleCellClick}
+        activePlayer={activePlayer}
+        players={players}
+        result={gameResult}
+        winner={winner}
+        onResetClick={this.handleResetButtonClick}
+      />
+    );
+  }
+
+  render() {
+    const { isGameStarted } = this.state;
+    return (
+      <div className="App">
+        <div id="main-container" className="container-fluid">
+          {isGameStarted ? this.renderGamePage() : this.renderWelcomePage()}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/gameActions.js
@@ -0,0 +1,36 @@
+export const makeEmptyGameField = size => Array(size).fill()
+  .map(() => Array(size).fill(null));
+
+export const makeStep = (row, cell, field, sign) => {
+  const newField = field.slice();
+  newField[row][cell] = sign;
+  return newField;
+}
+
+export const isCellTaken = (row, cell, field) => !!field[row][cell];
+
+export const isGameOver = (field, fieldSize, sign) => {
+  for (let x = 0; x < fieldSize; x += 1) {
+    let horizontal = true;
+    let vertical = true;
+    for (let y = 0; y < fieldSize; y += 1) {
+      horizontal = horizontal && field[x][y] === sign;
+      vertical = vertical && field[y][x] === sign;
+    }
+    if (horizontal || vertical) {
+      return true;
+    }
+  }
+
+  let diagonalUp = true;
+  let diagonalDown = true;
+
+  for (let i = 0; i < fieldSize; i += 1) {
+    diagonalDown = diagonalDown && field[i][i] === sign;
+    diagonalUp = diagonalUp && field[fieldSize - 1 - i][i] === sign;
+  }
+
+  return diagonalUp || diagonalDown;
+};
+
+export const isTie = movesCounter => movesCounter === 9;
